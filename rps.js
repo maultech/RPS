8af0bfd9212b8fbfffa163a5c9dfc6a7fdd828b9
maultech <hilowh@yahoo.com>
2026-01-05 07:20:36 -0800

    Start of app from memory

---
+++ rps.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    const compChoice = Math.floor(Math.random() * 3 + 1);
+    console.log(compChoice);
+    return compChoice;
+}
+
+function getHumanChoice(){
+    const userChoice = prompt('1, 2 or 3 for rock, paper or scissors?');
+    console.log(userChoice);
+    return userChoice;
+}
+
+function calculateWinLoss(user, computer){
+    if (computer === 1 && user === )
+}
+getComputerChoice();
+getHumanChoice();
