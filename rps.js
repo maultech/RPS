24391a520e1acd31a6e3411b2b17b340621229ad
maultech <hilowh@yahoo.com>
2026-01-05 11:41:12 -0800

    'tallyGames() doesn't work, doesn't receive any parameters

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
 function getComputerChoice(){
     const compChoice = Math.floor(Math.random() * 3 + 1);
     console.log('Computer choice: '+ compChoice);
@@ -5,23 +9,54 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    const userChoice = parseInt(prompt('1, 2 or 3 for rock, paper or scissors?'));
+    const userChoice = prompt('Enter your choice of Rock, Paper or Scissors:');
+    userChoice.toLowerCase;
     console.log('User choice ' + userChoice);
     return userChoice;
 }
 
 
-function calculateWinLoss(computer, user){
-    console.log('Computer: ' + computer + ', User:' + user);
-    if (computer === 1 && user === 3 || computer === 2 && user === 1){
-        console.log('Sorry, you lose');
-    } else if (computer === 1 && user === 2 || computer === 2 && user === 3){
-        console.log('Yay, you win!');
-    } else if (computer === 3 && user === 2){
-        console.log('Sorry, you lose');
-    } else if (computer === 3 && user === 1){
-        console.log('Yay, you win, rock breaks scissors!');
-    } else console.log('Game is tied!');
+function playRound(computer, user){
+    roundNumber++;
+    console.log('Computer: ' + computer + ', User: ' + user);
+    if (computer === 1 && user === 'scissors' || computer === 2 && user === 'rock'){
+        console.log(`Sorry, you lose round #: ${roundNumber}`);
+        computerScore += 1;
+        console.log('computerScore is ' + computerScore +', humanScore is ' + humanScore);
+    } else if (computer === 1 && user === 'paper' || computer === 2 && user === 'scissors'){
+        console.log(`Yay, you win round #: ${roundNumber}!`);
+        humanScore += 1;
+    } else if (computer === 3 && user === 'paper'){
+        console.log(`Sorry, you lose round #: ${roundNumber}`);
+        computerScore += 1;
+        console.log('computerScore is ' + computerScore + ', humanScore is ' + humanScore);
+    } else if (computer === 3 && user === 'rock'){
+        console.log(`Yay, you win round #: ${roundNumber}, rock breaks scissors!`);
+        humanScore += 1;
+    } else if (computer === user){
+        console.log(`Game is tied at round #: ${roundNumber}!`);
+    }
+    return humanScore, computerScore;
 }
 
-calculateWinLoss(getComputerChoice(), getHumanChoice());
+function playGame(){
+    playRound(getComputerChoice(), getHumanChoice());
+    playRound(getComputerChoice(), getHumanChoice());
+    playRound(getComputerChoice(), getHumanChoice());
+    playRound(getComputerChoice(), getHumanChoice());
+    playRound(getComputerChoice(), getHumanChoice());
+}
+
+function tallyGames(){
+    let win = '';
+    if (human > computer){
+        win = 'Computer is toast! Congrats!';
+    } else if (human < computer){
+        win = 'Computer beat you!';
+    } else if (human === computer){
+        win = 'Total game score is tied';
+    }
+}
+
+playGame(playRound());
+tallyGames();
